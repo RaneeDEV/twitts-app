024acf94f716537ff0757dc3e375a98c1c7509a2
Dmitry <67798875+RaneeDEV@users.noreply.github.com>
2021-05-31 19:12:44 +0300

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
 import "./App.css";
-import PostsList from "./components/PostsList/PostsList"
-import PostPage from "./components/PostPage/PostPage"
-import UserPage from "./components/UserPage/UserPage"
-import ErrorPage from "./components/ErrorPage/ErrorPage"
+import PostsList from "./components/PostsList/PostsList";
+import PostPage from "./components/PostPage/PostPage";
+import UserPage from "./components/UserPage/UserPage";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 import { Switch, Route, Link } from "react-router-dom";
+import "antd/dist/antd.css";
+import { PageHeader, Button } from "antd";
 
 function App() {
   return (
     <>
-        <div className="container">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">PostList</Link>
-              </li>
-              <li>
-                <Link to="/post/2">Post</Link>
-              </li>
-              <li>
-                <Link to="/user/5">User</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path="/">
-              <PostsList/>
-            </Route>
-            <Route exact path="/post/:postId">
-              <PostPage />
-            </Route>
-            <Route exact path="/user/:userId">
-              <UserPage />
-            </Route>
-            <Route path="*">
-              <ErrorPage />
-            </Route>
-          </Switch>
+      <div className="container">
+        <div className="site-page-header-ghost-wrapper">
+          <PageHeader
+            ghost={false}
+            onBack={() => window.history.back()}
+            title="Title"
+            extra={[
+              <Button key="1" type="primary">
+                <Link to="/">Post List</Link>
+              </Button>,
+              ,
+            ]}
+          ></PageHeader>
         </div>
+        <Switch>
+          <Route exact path="/">
+            <PostsList />
+          </Route>
+          <Route exact path="/post/:postId">
+            <PostPage />
+          </Route>
+          <Route exact path="/user/:userId">
+            <UserPage />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </div>
     </>
   );
 }
