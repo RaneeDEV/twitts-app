49ac5ed8b2920ef9b79dad0ad9398f7250d4e7a0
Dmitry <67798875+RaneeDEV@users.noreply.github.com>
2021-06-01 17:41:00 +0300

    Add styles

--- src/components/PostPage/PostPage.jsx
+++ src/components/PostPage/PostPage.jsx
@@ -1,25 +1,44 @@
-import { useParams, useHistory } from "react-router-dom";
+import { useParams, useHistory, Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import getData from '../../api/getData'
+import getData from "../../api/getData";
+import "./PostPage.css";
+import { Comment, Typography } from "antd";
+const { Text } = Typography;
 
 function UserPage() {
   const { postId } = useParams();
   const history = useHistory();
   const [post, setPost] = useState([]);
   useEffect(() => {
-    getData(`https://jsonplaceholder.typicode.com/posts/${postId}?_expand=user&_embed=comments`, data => setPost(data));
+    getData(
+      `https://jsonplaceholder.typicode.com/posts/${postId}?_expand=user&_embed=comments`,
+      (data) => setPost(data)
+    );
   }, [postId]);
   return (
     <>
-      <h1 className="king-title">Post Page</h1>
-      <p>{post.user?.name}</p>
-      <p>{post.title}</p>
-      <p>{post.body}</p>
-      <hr />
-      {post.comments?.map(comment => <p key={comment.id}>{comment.body}</p>)}
+      <div className="user-post">
+        <h1 className="king-title">Post Page</h1>
+        <Link to={`/user/${post.user?.id}`}>{post.user?.name}</Link>
+        <Text>
+          <p>{post.title}</p>
+        </Text>
+        <Text type="secondary">
+          <p>{post.body}</p>
+        </Text>
+      </div>
+      <h2 className="comments-title">{post.comments?.length} comments</h2>
+      {post.comments?.map((comment) => (
+        <Comment
+          content={
+            <Text type="secondary">
+              <p>{comment.body}</p>
+            </Text>
+          }
+        />
+      ))}
     </>
   );
-
 }
 
 export default UserPage;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,22 +1,41 @@
 import { useParams } from "react-router-dom";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import getData from "../../api/getData";
-
+import { Card, Typography } from "antd";
+const { Link, Text } = Typography;
 
 function UserPage() {
   const { userId } = useParams();
   const [user, setUser] = useState({});
   useEffect(() => {
-    getData(`https://jsonplaceholder.typicode.com/users/${userId}`, data => setUser(data));
+    getData(`https://jsonplaceholder.typicode.com/users/${userId}`, (data) =>
+      setUser(data)
+    );
   }, [userId]);
   return (
     <>
-      <h1 className="king-title">UserPage</h1>
-      <p>{user.name}</p>
-      <p>{user.username}</p>
-      <p>{user.company?.name}</p>
-      <p>{user.email}</p>
-      <p>{user.phone}</p>
+      <h1 className="king-title">User Page</h1>
+      <Card title={user.name} bordered={false}>
+        <Text mark strong>Username:</Text> {user.username}
+        <hr />
+        <Text mark strong>Company:</Text> {user.company?.name}: "
+        {user.company?.catchPhrase} ..."
+        <hr />
+        <Text mark strong>Address:</Text> {user.address?.city} - {user.address?.street}
+        , {user.address?.zipcode}
+        <hr />
+        <Text mark strong>Phone Number:</Text>{" "}
+        <Link href={`tel:${user?.phone}`}>{user.phone}</Link>
+        <hr />
+        <Text mark strong>Email:</Text>{" "}
+        <Link href={`mailto:${user?.email}`}>{user.email}</Link>
+        <hr />
+        <Text mark strong>WebSite:</Text>{" "}
+        <Link href={`https://${user?.website}`} target="_blank">
+          {user.website}
+        </Link>
+        <hr />
+      </Card>
     </>
   );
 }
